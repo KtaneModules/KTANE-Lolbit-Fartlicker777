6b0e048974d9f3a6fd1a40a2da5775611ecfd993
Fartlicker777 <asusshadows7172@gmail.com>
2022-03-01 00:55:40 -0600

    Final build

--- Lolbit/Assets/Lolbit.cs
+++ Lolbit/Assets/Lolbit.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
-using KModkit;
 using Rnd = UnityEngine.Random;
 
 public class Lolbit : MonoBehaviour {
@@ -13,7 +9,9 @@ public class Lolbit : MonoBehaviour {
    public KMAudio Audio;
    public KMNeedyModule Needy;
 
-   public Material[] Statics;
+   public Renderer[] Statics;
+
+   public AudioSource Music;
 
    bool Stupidthingactive;
 
@@ -23,54 +21,76 @@ public class Lolbit : MonoBehaviour {
 
    bool[] Typed = new bool[3];
 
-   Coroutine Waiting;
    Coroutine Active;
    Coroutine PleaseStandBy;
 
-   static int ModuleIdCounter = 1;
-   int ModuleId;
-   private bool ModuleSolved;
+   //Music
+   static bool Playing;
+   private float DefaultGameMusicVolume;
 
    void Awake () {
-      ModuleId = ModuleIdCounter++;
 
       Needy.OnNeedyActivation += OnNeedyActivation;
+      try {
+         DefaultGameMusicVolume = GameMusicControl.GameMusicVolume;
+         if (!Playing) {
+            Music.Play();
+            Playing = true;
+         }
+      }
+      catch (Exception) { }
+      try { GameMusicControl.GameMusicVolume = 0.0f; } catch (Exception) { }
+
+      GetComponent<KMGameInfo>().OnStateChange += state => {
+         if (state == KMGameInfo.State.Transitioning) {
+            Playing = false;
+            try {
+               Music.Stop();
+            }
+            catch {
+               throw new Exception("YOU MUST CONSTRUCT ADDITIONAL PYLONS");
+            }
+            finally {
+               GameMusicControl.GameMusicVolume = DefaultGameMusicVolume;
+            }
+         }
+      };
 
       StartCoroutine(Static());
       LOL.SetActive(false);
       for (int i = 0; i < 3; i++) {
          Lolbits[i].SetActive(false);
       }
-      Debug.Log("C");
    }
 
    protected void OnNeedyActivation () {
       Stupidthingactive = true;
-      Debug.Log("A");
-      Waiting = StartCoroutine(Wait());
+      StartCoroutine(ChangeTime());
+      StartCoroutine(Wait());
    }
 
    IEnumerator Wait () {
-      Debug.Log("L");
-      yield return new WaitForSeconds(Rnd.Range(20, 30));
+      Started = false;
+      yield return new WaitForSeconds(Rnd.Range(20, 31));
       Started = true;
       Active = StartCoroutine(Activation());
    }
 
    IEnumerator Activation () {
       
-      for (int i = 0; i < 3; i++) {
-         yield return new WaitForSeconds(5f);
+      for (int i = 0; i < 2; i++) {
          Lolbits[i].SetActive(true);
+         yield return new WaitForSeconds(1f);
       }
+      Lolbits[2].SetActive(true);
       PleaseStandBy = StartCoroutine(GetLolled());
    }
 
    IEnumerator GetLolled () {
-      for (int i = 0; i < 21; i++) {
+      LOL.SetActive(true);
+      while (true) {
          Audio.PlaySoundAtTransform("LOLnoise", transform);
-         LOL.SetActive(true);
-         yield return new WaitForSeconds(.48f);
+         yield return new WaitForSeconds(.17f);
       }
    }
 
@@ -82,52 +102,90 @@ public class Lolbit : MonoBehaviour {
          a += .01f;
          b += .01f;
          c += .01f;
-         Statics[0].SetTextureOffset("_MainTex", new Vector3(a, Rnd.Range(0f, 1f)));
-         Statics[1].SetTextureOffset("_MainTex", new Vector3(b, Rnd.Range(0f, 1f)));
-         Statics[2].SetTextureOffset("_MainTex", new Vector3(c, Rnd.Range(0f, 1f)));
-         yield return new WaitForSeconds(.01f);
+         Statics[0].material.SetTextureOffset("_MainTex", new Vector3(a, Rnd.Range(0f, 1f)));
+         Statics[1].material.SetTextureOffset("_MainTex", new Vector3(b, Rnd.Range(0f, 1f)));
+         Statics[2].material.SetTextureOffset("_MainTex", new Vector3(c, Rnd.Range(0f, 1f)));
+         yield return new WaitForSeconds(.03f);
+      }
+   }
+
+   IEnumerator ChangeTime () {
+      int Iteration = 0;
+      int[] WASTHATTHEBTIEDIOAWNDOWNAODWNOW = { 1, 9, 8, 3};
+      while (true) {
+         Needy.SetNeedyTimeRemaining(WASTHATTHEBTIEDIOAWNDOWNAODWNOW[Iteration % 4] * 10 + WASTHATTHEBTIEDIOAWNDOWNAODWNOW[(Iteration + 1) % 4]);
+         yield return new WaitForSeconds(1f);
+         Iteration++;
       }
    }
 
    void Update () {
       if (Stupidthingactive) {
-         Needy.SetNeedyTimeRemaining(83);
+         Needy.SetNeedyTimeRemaining((float) Math.Ceiling(Needy.GetNeedyTimeRemaining()));
       }
       if (Started) {
          if (Input.GetKeyDown(KeyCode.L) && !Typed[0]) {
             Typed[0] = true;
+            Audio.PlaySoundAtTransform("LType", transform);
          }
-         else if (Input.GetKeyDown(KeyCode.O) && !Typed[0]) {
+         else if (Input.GetKeyDown(KeyCode.O) && Typed[0] && !Typed[1]) {
             Typed[1] = true;
+            Audio.PlaySoundAtTransform("OType", transform);
          }
-         else if (Input.GetKeyDown(KeyCode.L) && !Typed[2] && Typed[0]) {
+         else if (Input.GetKeyDown(KeyCode.L) && !Typed[2] && Typed[0] && Typed[1]) {
             Typed[2] = true;
+            Audio.PlaySoundAtTransform("LType", transform);
          }
          if (Typed[0] && Typed[1] && Typed[2]) {
-            if (Active != null) {
-               StopCoroutine(Active);
-            }
-            if (PleaseStandBy != null) {
-               StopCoroutine(PleaseStandBy);
-            }
-            for (int i = 0; i < 3; i++) {
-               Lolbits[i].SetActive(false);
-            }
-            Waiting = StartCoroutine(Wait());
+            Reset();
          }
       }
+   }
+
+   void Reset () {
+      if (Active != null) {
+         StopCoroutine(Active);
+      }
+      if (PleaseStandBy != null) {
+         StopCoroutine(PleaseStandBy);
+      }
+      for (int i = 0; i < 3; i++) {
+         Lolbits[i].SetActive(false);
+         Typed[i] = false;
+      }
+      LOL.SetActive(false);
+      StartCoroutine(Wait());
    }
    
 
 #pragma warning disable 414
-   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+   private readonly string TwitchHelpMessage = @"Use !{0} LOL. Think next time before you use !{0} help.";
 #pragma warning restore 414
 
    IEnumerator ProcessTwitchCommand (string Command) {
-      yield return null;
+      Command = Command.Trim().ToUpper();
+      if (Command == "LOL" && Started) {
+         Audio.PlaySoundAtTransform("LType", transform);
+         yield return new WaitForSeconds(.1f);
+         Audio.PlaySoundAtTransform("OType", transform);
+         yield return new WaitForSeconds(.1f);
+         Audio.PlaySoundAtTransform("LType", transform);
+         yield return new WaitForSeconds(.1f);
+         Reset();
+      }
+      else {
+         yield return "sendtochaterror How the fuck.";
+      }
    }
 
    IEnumerator TwitchHandleForcedSolve () {
-      yield return null;
+      while (true) {
+         if (!Started) {
+            yield return true;
+         }
+         else {
+            yield return ProcessTwitchCommand("LOL");
+         }
+      }
    }
 }
